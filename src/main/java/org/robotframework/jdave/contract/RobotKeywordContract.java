794b7ceec290c39cc0326d07364c9fda6f19b22c
Jussi Malinen <jussi.ao.malinen@gmail.com>
2013-03-12 13:31:21 +0200

    support for overload annotation in javalibcore 1.1

--- src/main/java/org/robotframework/jdave/contract/RobotKeywordContract.java
+++ src/main/java/org/robotframework/jdave/contract/RobotKeywordContract.java
@@ -1,11 +1,14 @@
 package org.robotframework.jdave.contract;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import jdave.ExpectationFailedException;
 import jdave.IContract;
 
 import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywordOverload;
 
 public class RobotKeywordContract implements IContract {
     private final String methodName;
@@ -15,17 +18,32 @@ public class RobotKeywordContract implements IContract {
     }
 
     public void isSatisfied(Object obj) throws ExpectationFailedException {
-        if (!findMethod(obj).isAnnotationPresent(RobotKeyword.class)) {
+        Method[] methods = findMethod(obj);
+        boolean keywordAnnotationPresent = false;
+        for (Method method: methods) {
+            if (method.isAnnotationPresent(RobotKeyword.class)) {
+                if (keywordAnnotationPresent) {
+                    throw new ExpectationFailedException(methodName + " is annotated twice with @RobotKeyword");
+                }
+                keywordAnnotationPresent = true;
+            } else if (!method.isAnnotationPresent(RobotKeywordOverload.class)) {
+                throw new ExpectationFailedException("All instances of "+ methodName + " must be annotated either with @RobotKeyword or @RobotKeywordOverload");
+            }
+        }
+        if (!keywordAnnotationPresent) {
             throw new ExpectationFailedException(methodName + " is not annotated with @RobotKeyword");
         }
     }
 
-    private Method findMethod(Object obj) {
+    private Method[] findMethod(Object obj) {
+        List<Method> results = new ArrayList<Method>();
         for (Method method : obj.getClass().getMethods()) {
             if (method.getName().equals(methodName)) {
-                return method;
+                results.add(method);
             }
         }
-        throw new ExpectationFailedException(methodName + " could not be found from " + obj.getClass().getName());
+        if (results.isEmpty())
+            throw new ExpectationFailedException(methodName + " could not be found from " + obj.getClass().getName());
+        return results.toArray(new Method[results.size()]);
     }
 }
